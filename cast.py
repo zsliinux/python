064dfca5d95edec6c0849be6ab796a8949311f6d
zzslinux <zzslinux@gmail.com>
2013-01-17 23:03:49 +0800

    1

---
+++ cast.py
@@ -0,0 +1,10 @@
+#from list import print_lol
+import list
+cast = ["palin",["cleese"],"ldle",["jones"]]
+movies = ["The Holy Grail",1975,"The meaning of Life",91,
+          ["graham chapman",
+           ["michael palin","john cleese","terry gilliam"]]]
+
+
+
+list.print_lol(cast,0)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name    = 'nester',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'zzslinux',
+        author_email = 'zzslinux@gmail.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
